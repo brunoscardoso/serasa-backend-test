a83503927072a1dbea1393fdd4639288ed85aeeb
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2020-03-19 19:29:07 -0300

    add heroku

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,11 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true,
+  },
+  host: process.env.DATABASE_URL,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
