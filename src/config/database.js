0785a56c0603b88c54cc304de89672d133844bbb
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2020-03-19 19:43:48 -0300

    add heroku

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
   dialectOptions: {
     ssl: true,
   },
-  host: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
